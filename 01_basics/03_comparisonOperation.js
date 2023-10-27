691b4a83d4c5e4bb705a1547fa34128ed91fa978
Sonali singh <101523856+curiousona@users.noreply.github.com>
2023-10-27 20:14:16 +0000

    updated

--- 01_basics/03_comparisonOperation.js
+++ 01_basics/03_comparisonOperation.js
@@ -24,3 +24,44 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+// *****************operation*****************
+
+let value = 45
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2 + 2);
+//console.log(2 - 2);
+//console.log(2 * 2);
+//console.log(2 ** 2);
+//console.log(2 / 2);
+//console.log(2 % 2);
+
+let str1 = "Sonali"
+let str2 = " codes"
+
+let str3 = str1 + str2
+console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//comsole.log("1" + "2");
+//console.log(1 + 2 + "2");
+//console.log("1" + 2 + 2)'
+
+//console.log(true);
+//console.log(+true);
+//console.log(+"");
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+/* link to study
+https://tc39.es/ecma262/#sec-abstract-operations*/
+
+
+
+
