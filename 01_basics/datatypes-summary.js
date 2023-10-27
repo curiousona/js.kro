825c6ccbd0982c48d067ca2f0c87f10d23a6c12b
Sonali singh <101523856+curiousona@users.noreply.github.com>
2023-10-27 21:13:22 +0000

    change in location of file

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,32 @@
+// Primitive
+
+// 7 types:- String, Boolean, Number, null, undefine, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmailId;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id == anotherId);
+
+const BigNumbers = 35364586476474787n
+
+// Reference (Non-Primitive)
+// Array, Objects, Functions
+
+const heros = ["Shaktiman", "Power Rangers", "Avengers" ];
+let myobj={
+    name: "Sonali",
+    favColor: "Purple",
+}
+
+const myFunction = function(){
+    console.log("Peekaboo");
+}
+console.log(typeof BigNumbers);
+
+
